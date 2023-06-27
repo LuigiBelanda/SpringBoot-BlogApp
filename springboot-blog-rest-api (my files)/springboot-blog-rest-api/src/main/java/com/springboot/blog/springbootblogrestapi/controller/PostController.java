cca82a204183e2096940d163aa2aa287a24e1c69
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-06-27 16:08:31 -0300

    PUT and DELETE route

--- springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -36,4 +36,17 @@ public class PostController {
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
+
+    // Update post by id rest api
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+        PostDto postResponse = postService.updatePost(postDto, id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
+    }
 }
--- springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -6,10 +6,12 @@ import com.springboot.blog.springbootblogrestapi.payload.PostDto;
 import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
 import com.springboot.blog.springbootblogrestapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class PostServiceImpl implements PostService {
     private PostRepository postRepository;
 
@@ -41,6 +43,27 @@ public class PostServiceImpl implements PostService {
     public PostDto getPostById(long id) {
         Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
         return mapToDTO(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        // Get post by id from the database
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatedPost = postRepository.save(post);
+        return mapToDTO(updatedPost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        // Get post by id from the database
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+
+        postRepository.delete(post);
     }
 
     // Convert Entity to DTO
