b321193ee4f4d07e30c971d9e3c0e40c6458bd48
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-06-27 17:12:09 -0300

    Ordering in Sorting API - ASC and DESC and Refactoring Hardcoded Values for Paging and Sorting

--- springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -38,7 +38,9 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
         Page<Post> posts = postRepository.findAll(PageRequest.of(pageNo, pageSize, Sort.by(sortBy)));
 
         // Get content for page object
