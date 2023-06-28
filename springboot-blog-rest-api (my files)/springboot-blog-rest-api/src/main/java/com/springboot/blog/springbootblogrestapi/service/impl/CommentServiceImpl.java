3735a7a35730d6303ca5d331dc829c8e8a8c2c5d
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-06-28 13:25:02 -0300

    Get especific Comment to especific Post

--- springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
@@ -2,12 +2,14 @@ package com.springboot.blog.springbootblogrestapi.service.impl;
 
 import com.springboot.blog.springbootblogrestapi.entity.Comment;
 import com.springboot.blog.springbootblogrestapi.entity.Post;
+import com.springboot.blog.springbootblogrestapi.exception.BlogAPIException;
 import com.springboot.blog.springbootblogrestapi.exception.ResourceNotFoundException;
 import com.springboot.blog.springbootblogrestapi.payload.CommentDto;
 import com.springboot.blog.springbootblogrestapi.repository.CommentRepository;
 import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
 import com.springboot.blog.springbootblogrestapi.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -47,6 +49,21 @@ public class CommentServiceImpl implements CommentService {
 
         // Convert list of comments entities to list of comment dto's
         return comments.stream().map(comment -> mapToDTO(comment)).collect(Collectors.toList());
+    }
+
+    @Override
+    public CommentDto getCommentById(Long postId, Long commentId) {
+        // Retrieve post entity by ud
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        // Retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment", "Id", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to post");
+        }
+
+        return mapToDTO(comment);
     }
 
     // Entity to DTO
