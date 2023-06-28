afe36cc8013bf9608f73c6629cd18bcb177bf07d
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-06-28 13:51:22 -0300

    DELETE Comment

--- springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
@@ -86,6 +86,21 @@ public class CommentServiceImpl implements CommentService {
         return mapToDTO(updatedComment);
     }
 
+    @Override
+    public void deleteComment(Long postId, Long commentId) {
+        // Retrieve post entity by ud
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        // Retrieve comment by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment", "Id", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to post");
+        }
+
+        commentRepository.delete(comment);
+    }
+
     // Entity to DTO
     private CommentDto mapToDTO(Comment comment) {
         CommentDto commentDto = new CommentDto();
