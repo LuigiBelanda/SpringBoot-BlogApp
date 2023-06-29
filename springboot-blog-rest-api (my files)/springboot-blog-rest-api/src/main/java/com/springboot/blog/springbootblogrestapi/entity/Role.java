dc249a5e33c22374c0fe7b8858d9d3e3737c4da6
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-06-29 14:26:57 -0300

    Relation Many to Many

---
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/entity/Role.java
@@ -0,0 +1,22 @@
+package com.springboot.blog.springbootblogrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.internal.util.stereotypes.ThreadSafe;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
