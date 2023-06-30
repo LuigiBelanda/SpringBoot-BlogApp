0f49b90aeb3f70504afd1446236e11e05fb3c1fd
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-06-30 15:17:35 -0300

    Define API information using annotations

--- springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
+++ springboot-blog-rest-api (my files)/springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,34 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App Rest APIs",
+				description = "Spring Boot Blog App REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Luigi",
+						email = "luigi.belanda.milani@gmail.com"
+				),
+				license = @License(
+						name = "MIT"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/LuigiBelanda/SpringBoot-BlogApp"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
